f90c20d1469a4cc78af378283bc1fe80f3c52641
mmacphail <alexandre.fruchaud@gmail.com>
2022-12-05 00:00:38 +0100

    --wip-- [skip ci]

--- src/main/java/eu/macphail/ExampleRoute.java
+++ src/main/java/eu/macphail/ExampleRoute.java
@@ -1,2 +1,15 @@
-package eu.macphail;public class ExampleRoute {
+package eu.macphail;
+
+import org.apache.camel.builder.RouteBuilder;
+
+import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.kafka;
+import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.timer;
+
+public class ExampleRoute extends RouteBuilder {
+
+  @Override
+  public void configure() throws Exception {
+    from(timer("foo").period(1000))
+          .to(kafka("test").brokers("kafka-1:19092"));
+  }
 }
--- src/test/java/eu/macphail/ExampleResourceIT.java
+++ src/test/java/eu/macphail/ExampleResourceIT.java
@@ -4,5 +4,5 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
 public class ExampleResourceIT extends ExampleResourceTest {
-    // Execute the same tests but in packaged mode.
+  // Execute the same tests but in packaged mode.
 }
